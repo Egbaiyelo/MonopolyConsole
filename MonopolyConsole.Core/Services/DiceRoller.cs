007c8440be4b922a1b72bfa9d3ea023942c066d7
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-10-28 10:00:00 -0400

    Update: Adding services

---
+++ MonopolyConsole.Core/Services/DiceRoller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyConsole.Core.Services
+{
+    public class DiceRoller
+    {
+        Random _random = new Random();
+
+        int Val1;
+        int Val2;
+        public int Total { get => Val1 + Val2; }
+        bool IsDoubles { get => Val1 == Val2; }
+
+        public DiceRoller() { }
+
+        public int Roll()
+        {
+            Val1 = _random.Next(7);
+            Val2 = _random.Next(7);
+            return Total;
+        }
+    }
+}
---
+++ MonopolyConsole.Core/Services/GameEngine.cs
@@ -0,0 +1,49 @@
+﻿using MonopolyConsole.Core.Interfaces;
+using MonopolyConsole.Core.Models;
+
+namespace MonopolyConsole.Core.Services
+{
+    public class GameEngine : IGameEngine
+    {
+        public void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        void IGameEngine.BuyProperty(Player player, Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IGameEngine.CanAffordProperty(Player player, Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        Player IGameEngine.GetCurrentPlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        Player IGameEngine.GetPlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IGameEngine.ProcessLanding(Player player, Tile tile)
+        {
+            //If tile is tax tax player, action, then do that, property then get type
+        }
+
+        void IGameEngine.ProcessTurn(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
