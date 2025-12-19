d6755aa9ab994ae628576bb948738c627793095b
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-12-19 11:27:34 -0500

    Update: Game Runner Structure
    Working on the gamerunner and game engine logic for setup

--- MonopolyConsole.Main/GameEngine.cs
+++ MonopolyConsole.Main/GameEngine.cs
@@ -15,20 +15,20 @@ namespace MonopolyConsole.App
     internal class GameEngine : IGameEngine
     {
         DiceRoller DiceRoller;
-        //IPrompter ConsolePrompter;
+        IPrompter ConsolePrompter;
         Board Board;
         GameDataService GameDataService;
 
         public GameEngine()
         {
-            SetupGame();
+            //SetupGame();
         }
 
 
-        public void SetupGame()
+        public void SetupGame(IPrompter prompter)
         {
             DiceRoller = new DiceRoller();
-            //ConsolePrompter = new HumanConsolePrompter();
+            ConsolePrompter = prompter;
             GameDataService = new GameDataService();
 
             // Board setup
@@ -39,16 +39,29 @@ namespace MonopolyConsole.App
         public void ProcessTurn(Player player)
         {
             int roll = DiceRoller.Roll();
+            ConsolePrompter.Notify(player, $"You rolled a {roll}");
 
-            MovePlayer(player, roll);   
-        }   
-        
+            MovePlayer(player, roll);
+
+            // Ask player what they want to do next?
+        }
+
         public void ProcessLanding(Player player, Tile tile)
         {
             switch (tile.Type) {
                 case Tile.TileType.Tax:
                     break;
                 case Tile.TileType.Property:
+                    if (tile.Deed.Owner != null)
+                    {
+                        HandlePayment(player, tile.Deed.Owner, tile.Deed.Rent);
+                    }
+                    else if (CanBuyProperty(player, tile.Deed))
+                    {
+                        
+                    }
+                    break;
+                case Tile.TileType.Card:
                     break;
 
                 // Jail (Just visiting), free parking
@@ -67,9 +80,17 @@ namespace MonopolyConsole.App
             throw new NotImplementedException();
         }
 
-        public bool CanAffordProperty(Player player, Property property)
+        public bool CanBuyProperty(Player player, Property property)
         {
-            throw new NotImplementedException();
+            if (property == null)
+                return false;
+
+            if (property.Owner != null)
+                return false;
+
+            if (property.Price > player.Balance)
+                return false;
+            
         }
 
         public void EndTurn()
@@ -109,9 +130,17 @@ namespace MonopolyConsole.App
 
         public void MovePlayer(Player player, int steps)
         {
+            // If player passed go
+            if (player.Position + steps > Board.BoardSize)
+            {
+                player.Balance += 200;
+                ConsolePrompter.Notify(player, $"You get $200!");
+            }
+
             // Update Player position
             player.Position += steps;
             player.Position %= Board.BoardSize;
+            ConsolePrompter.Notify(player, $"You have landed on {Board[player.Position].Name}");
 
             // Handle landing
             ProcessLanding(player, Board[player.Position]);
@@ -119,7 +148,15 @@ namespace MonopolyConsole.App
 
         public void MovePlayerTo(Player player, int tileIndex)
         {
-            throw new NotImplementedException();
+            // If player passed go
+            if (player.Position > tileIndex)
+                player.Balance += 200;
+
+            // Update Player position
+            player.Position = tileIndex;
+
+            // Handle landing
+            ProcessLanding(player, Board[player.Position]);
         }
 
 
--- MonopolyConsole.Main/GameRunner.cs
+++ MonopolyConsole.Main/GameRunner.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MonopolyConsole.App.Prompters;
+using MonopolyConsole.Core.Interfaces;
+using MonopolyConsole.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +14,37 @@ namespace MonopolyConsole.App
         //load game, save game, 
 
         // ask for player number, names and setup game engine and game
-        void StartGame()
-        {
 
+        // deals with players, engine deals with game
+
+        Player[] Players;
+        IPrompter HumanPrompter;
+        IPrompter BotPrompter;
+
+        internal void StartGame()
+        {
+            int intResponse;
+
+            // How many players, names
+            Console.WriteLine("How many players are in the game => ");
+            intResponse = int.Parse(Console.ReadLine());
+            Players = new Player[intResponse];
+
+            // How many bots - none for now
+            //Console.WriteLine("How many bots are in the game => ");
+            //intResponse = int.Parse(Console.ReadLine());
+            //Players = new Player[intResponse];
+
+            HumanPrompter = new HumanConsolePrompter();
+
+            // Introduce themselves, if bots, then bots get names too later in different loop
+            for (int i = 0; i < Players.Length; i++) 
+            {
+                // ask name
+                Console.WriteLine($"Player {i}, What is your name => ");
+                Players[i] = new Player();
+                Players[i].Name = 
+            }
         }
 
         // acknowledge players, loop turns
